d0cc368189491c1c351b24173e7891b47a41bac4
Emilio Coppa <ercoppa@gmail.com>
2019-12-27 19:44:47 +0100

    symbolic struct

---
+++ tcg/symbolic/symbolic-struct.h
@@ -0,0 +1,83 @@
+#ifndef SYMBOLIC_STRUCT_H
+#define SYMBOLIC_STRUCT_H
+
+typedef enum OPKIND
+{
+    RESERVED,
+    // op1 is used to store the value or id
+    IS_CONST, // constants could also be embedded within an operand
+    IS_SYMBOLIC,
+    // unary
+    NEG,
+    // binary
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    DIVU,
+    REM,
+    REMU,
+    AND,
+    OR,
+    XOR,
+    SHL,
+    SHR,
+    SAR,
+    ROTL,
+    ROTR,
+    //
+    EQ,
+    NE,
+    // signed
+    LT,
+    LE,
+    GE,
+    GT,
+    // unsigned
+    LTU,
+    LEU,
+    GEU,
+    GTU,
+    // binary
+    ZEXT, // ZEXT(arg0, n): zero-extend arg0 from the n-1 msb bits
+    SEXT, // SEXT(arg0, n): sign-extend arg0 from the n-1 msb bits
+    // binary
+    CONCAT8,  // CONCAT8(arg0, arg1): concat one byte (arg1) to arg0
+    EXTRACT8, // EXTRACT8(arg0, i): extract i-th byte from arg0
+    // ternary
+    DEPOSIT,  // DEPOSIT(arg0, arg1, arg2, pos, len):
+              //    arg0 = (arg1 & ~MASK(pos, len)) | ((arg2 << pos) & MASK(pos, len))
+              // where: MASK(pos, len) build a mask of bits, where len bits are set to
+              // one starting at position 8 (going towards msb).
+              // e.g., MASK(8, 4) = 0x0f00
+    EXTRACT,  // EXTRACT(arg0, arg1, pos, len):
+              //    arg0 = (arg1 << (N_BITS-(pos+len))) >> (N_BITS-len)
+              // e.g., EXTRACT(arg0, arg1, 8, 4):
+              //  when N_BITS=32 then arg0 = (arg1 << 20) >> 28
+    SEXTRACT, // same as EXTRACT but using arithmetic shift
+} OPKIND;
+
+
+typedef enum EXTENDKIND
+{
+    ZEXT_8,
+    ZEXT_16,
+    ZEXT_32,
+    //
+    SEXT_8,
+    SEXT_16,
+    SEXT_32,
+} EXTENDKIND;
+
+typedef struct Expr
+{
+    struct Expr *op1;
+    struct Expr *op2;
+    struct Expr *op3;
+    uint8_t opkind;
+    uint8_t op1_is_const;
+    uint8_t op2_is_const;
+    uint8_t op3_is_const;
+} Expr;
+
+#endif // SYMBOLIC_STRUCT_H
--- tcg/symbolic/symbolic.c
+++ tcg/symbolic/symbolic.c
@@ -2,90 +2,13 @@
 
 #include "qemu/osdep.h"
 #include "qemu-common.h"
-#include "symbolic.h"
 #include "qemu/bitops.h"
 
+#include "symbolic.h"
 #include "config.h"
+#include "symbolic-struct.h"
 
 #define SYMBOLIC_DEBUG
-
-typedef enum OPKIND
-{
-    RESERVED,
-    // op1 is used to store the value or id
-    IS_CONST, // constants could also be embedded within an operand
-    IS_SYMBOLIC,
-    // unary
-    NEG,
-    // binary
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    DIVU,
-    REM,
-    REMU,
-    AND,
-    OR,
-    XOR,
-    SHL,
-    SHR,
-    SAR,
-    ROTL,
-    ROTR,
-    //
-    EQ,
-    NE,
-    // signed
-    LT,
-    LE,
-    GE,
-    GT,
-    // unsigned
-    LTU,
-    LEU,
-    GEU,
-    GTU,
-    // binary
-    ZEXT, // ZEXT(arg0, n): zero-extend arg0 from the n-1 msb bits
-    SEXT, // SEXT(arg0, n): sign-extend arg0 from the n-1 msb bits
-    // binary
-    CONCAT8,  // CONCAT8(arg0, arg1): concat one byte (arg1) to arg0
-    EXTRACT8, // EXTRACT8(arg0, i): extract i-th byte from arg0
-    // ternary
-    DEPOSIT,  // DEPOSIT(arg0, arg1, arg2, pos, len):
-              //    arg0 = (arg1 & ~MASK(pos, len)) | ((arg2 << pos) & MASK(pos, len))
-              // where: MASK(pos, len) build a mask of bits, where len bits are set to
-              // one starting at position 8 (going towards msb).
-              // e.g., MASK(8, 4) = 0x0f00
-    EXTRACT,  // EXTRACT(arg0, arg1, pos, len):
-              //    arg0 = (arg1 << (N_BITS-(pos+len))) >> (N_BITS-len)
-              // e.g., EXTRACT(arg0, arg1, 8, 4):
-              //  when N_BITS=32 then arg0 = (arg1 << 20) >> 28
-    SEXTRACT, // same as EXTRACT but using arithmetic shift
-} OPKIND;
-
-typedef enum EXTENDKIND
-{
-    ZEXT_8,
-    ZEXT_16,
-    ZEXT_32,
-    //
-    SEXT_8,
-    SEXT_16,
-    SEXT_32,
-} EXTENDKIND;
-
-typedef struct Expr
-{
-    struct Expr *op1;
-    struct Expr *op2;
-    struct Expr *op3;
-    uint8_t opkind;
-    uint8_t op1_is_const;
-    uint8_t op2_is_const;
-    uint8_t op3_is_const;
-} Expr;
 
 // symbolic temps
 Expr *s_temps[TCG_MAX_TEMPS] = {0};
