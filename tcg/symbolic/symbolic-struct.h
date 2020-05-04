045af1dfdb8d38e40a33fca92a144b28855708fe
Emilio Coppa <ercoppa@gmail.com>
2020-05-04 16:52:56 +0200

    fixes

--- tcg/symbolic/symbolic-struct.h
+++ tcg/symbolic/symbolic-struct.h
@@ -14,7 +14,7 @@
 #endif
 
 #define EXPR_POOL_CAPACITY  (1024 * 1024 * 8)
-#define EXPR_QUERY_CAPACITY (512 * 1024)
+#define EXPR_QUERY_CAPACITY (260 * 1024)
 #define EXPR_POOL_SHM_KEY   (0xDEADBEEF)
 #define EXPR_POOL_ADDR      ((const void*)0x7f05c8cc7000)
 #define QUERY_SHM_KEY       0xCAFECAFE
